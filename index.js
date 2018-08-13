203108b6e756e4fa14c88f8ff3133adee92007bc
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-13 22:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ if (whisper(string) === string){
   return "I can't hear you!"
 } else if (shout(string) === string){
   return "YES INDEED!"
-} else if (string === "I love you"
+} else if (string === "I love you, Grandma."){
 return "I love you too"
 }  
+}
 
