46b1f6ddbdc8618107649c2d63524f6ef3aa3677
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-13 22:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,5 @@ function logWhisper(string){
 function sayHiToGrandma(string){
 if (whisper(string) === string){
   return "I can't hear you!"
-} else (shout(string) === string){
-  return "YES INDEED!"
-}
+} 
 }
